dd951908ee63fa868e53418111858c98e9534696
daffarafip24 <daffarafip24@gmail.com>
2024-04-17 16:40:49 +0700

    Create Notification database and Notification repository struct skeleton.

---
+++ src/repository/notification.rs
@@ -0,0 +1,14 @@
+use std::sync::RwLock;
+
+use lazy_static::lazy_static;
+
+use crate::model::notification::Notification;
+
+// Singleton of Database
+lazy_static! {
+    pub static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(vec![]);
+}
+
+pub struct NotificationRepository;
+impl NotificationRepository {
+}
